8464e3941965aa237ba193155217f179867c0bfd
Sven BARRAY <svenbarray@gmail.com>
2024-02-01 11:59:35 +0100

    Ajout du code qui permet la création de l'index title.non_pos_index.json

--- main2.py
+++ main2.py
@@ -20,14 +20,13 @@ def calculate_token_statistics(tokens):
 def display_statistics(data, titles_tokens, fields_stats=False):
     """
     Fonction pour afficher les statistiques de nos données. 
-    Il y a une redondance des paramètres avec titles_tokens (issus de data), mais cela permet d'éviter de tokéniser les titres deux fois.
+    Il y a une redondance des paramètres avec titles_tokens (issus de data), mais cela permet d'éviter de tokeniser les titres deux fois.
     field_stats détermine si on veut également les statistiques du contenu et des headers. Si False, on aura seulement les données des titres. False par défaut car cette opération est coûteuse en temps
     """
     total_tokens_titles, avg_tokens_titles = calculate_token_statistics(titles_tokens)
 
     num_documents = len(data)
     df = pd.DataFrame({'TitleTokens': [len(t) for t in titles_tokens]})
-
 
     if fields_stats:
         contents = [doc['content'] for doc in data]
@@ -56,9 +55,29 @@ def display_statistics(data, titles_tokens, fields_stats=False):
     print(f"Distribution des tokens :")
     print(df.describe())
 
+def build_non_positional_index(titles_tokens):
+    """Fonction pour construire l'index non positionnel."""
+    index = {}
+    for doc_id, tokens in enumerate(titles_tokens):
+        for token in tokens:
+            if token not in index:
+                index[token] = []
+            index[token].append(doc_id)
+    return index
+
+def save_index_to_file(index, filename):
+    """Fonction pour enregistrer l'index dans un fichier."""
+    with open(filename, 'w', encoding='utf-8') as file:
+        json.dump(index, file, ensure_ascii=False)
+
+
 if __name__ == "__main__":
     path = 'crawled_urls.json'
     data = load_json_data(path)
     titles = [doc['title'] for doc in data]
     titles_tokens = tokenize_texts(titles)
+
     display_statistics(data, titles_tokens, fields_stats=False)
+
+    non_pos_index = build_non_positional_index(titles_tokens)
+    save_index_to_file(non_pos_index, 'title.non_pos_index.json')
